db8c315fcb79aa4a419de5844769ad0ba0ab0f2b
Kacper Kaseja <yamakaszi10@o2.pl>
2020-04-12 13:45:37 +0200

    Add contact sectiona and emai imput whit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Hamburger menu 
+
 const hamburgerIco = document.querySelector('.navigation__hamburger-ico');
 const menu = document.querySelector('.navigation__list');
 
@@ -11,3 +13,20 @@ const menuActive = () => {
 }
 
 hamburgerIco.addEventListener('click', hamburgerActive);
+
+//Hamburger Menu End
+
+//Rotate btn
+const emailBtn = document.querySelector('.contact__button');
+const emailIco = document.querySelector('.contact__button svg ');
+
+emailBtn.addEventListener('click', () => {
+
+    emailIco.style.transition = "2s";
+    emailIco.style.transform = "rotate(270deg) scale(0.8)";
+    // emailBtn.style.transition = "border-bottom 2s";
+    // emailBtn.style.border = "none";
+    emailBtn.classList.add('rotate');
+})
+
+//Rotate btn ends
