31a446b3739fc737c0eb57fc8f5bc46ddc0f9bed
Kacper Kaseja <yamakaszi10@o2.pl>
2020-04-13 02:44:32 +0200

    Add funcionality do adding new card

--- js/main.js
+++ js/main.js
@@ -54,35 +54,76 @@ menuLinks.forEach((item) => {
 
 //Menu shop-chart
 
-const chartButtons = [...document.querySelectorAll('.shop-chart__button')]
 
-let id;
 
-chartButtons.forEach(item => {
+const cardButtonChange = function () {
 
-    item.addEventListener('click', () => {
-        item.style.border = 'none';
-        item.style.transform = 'translateY(8px)';
-        id = window.setTimeout(() => {
-            item.style.transform = 'translateY(0px)';
-            item.style.borderBottom = '6px solid #DD6B44'
-        }, 1000)
+    const chartButtons = [...document.querySelectorAll('.shop-chart__button')]
+
+    let id;
+
+    chartButtons.forEach(item => {
+
+        item.addEventListener('click', () => {
+            item.style.border = 'none';
+            item.style.transform = 'translateY(8px)';
+            id = window.setTimeout(() => {
+                item.style.transform = 'translateY(0px)';
+                item.style.borderBottom = '6px solid #DD6B44'
+            }, 1000)
+        })
+
+        console.log(id);
+
+        item.addEventListener('mousedown', () => {
+            item.classList.add('cliced');
+            window.clearTimeout(id);
+        })
+        item.addEventListener('mouseup', () => {
+            window.clearTimeout(id);
+            item.classList.remove('cliced');
+        })
+
     })
 
-    console.log(id);
+}
 
+cardButtonChange();
 
+//Click Events To Buttons End's
 
-    item.addEventListener('mousedown', () => {
-        item.classList.add('cliced');
-        window.clearTimeout(id);
-    })
-    item.addEventListener('mouseup', () => {
-        window.clearTimeout(id);
-        item.classList.remove('cliced');
-    })
+//Adding nw chart to gallery
 
+const cardHtml = `
 
+<div class="photo">
+    <img class="shop-chart__photo" src="https://picsum.photos/200/300" alt="">
+</div>
+<div class="shop-chart__pricing">
+    <p class="dress-name">Mimosa Lace Dress</p>
+    <p class="dress-price">$76.95</p>
+    <button class="shop-chart__button">Add to chart <svg xmlns="http://www.w3.org/2000/svg"
+            width="37" height="37" viewBox="0 0 37 37">
+            <path id="Rectangle_23_copy" data-name="Rectangle 23 copy"
+                d="M571.06,696V682.94H558V672.06h13.06V659h10.88v13.06H595v10.88H581.94V696Z"
+                transform="translate(-558 -659)" fill="#fff" />
+        </svg></button>
+</div>
 
+`;
 
+const loadButton = document.querySelector('.gallery__more');
+const gallery = document.querySelector('.shop-chart__gallery');
+
+loadButton.addEventListener('click', () => {
+    console.log('kutsa')
+    const newCard = document.createElement('div');
+    newCard.classList.add('shop-chart__item');
+    newCard.innerHTML = cardHtml;
+    const newCard_2 = document.createElement('div');
+    newCard_2.classList.add('shop-chart__item');
+    newCard_2.innerHTML = cardHtml;
+    gallery.append(newCard);
+    gallery.append(newCard_2);
+    cardButtonChange();
 })
